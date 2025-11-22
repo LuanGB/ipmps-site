2dec5e92a7853988828d32bbc18a0ca6df089597
12vieira <mapivieira@gmail.com>
2025-11-22 17:33:23 -0300

    home root about contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
-
-
+  
   root to: 'site#home'
 
   get 'home', to: 'site#home', as: 'home'
